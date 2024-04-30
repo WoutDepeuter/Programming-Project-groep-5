fddf4af465e08f7827e8bcb656e29c6a8f0949b2
BCornet <brent.cornet@telenet.be>
2024-04-30 16:16:18 +0200

    Oplossen slider-probleem + oplossen imagepaths

--- Frontend/User-interface/HomeScreen.js
+++ Frontend/User-interface/HomeScreen.js
@@ -1,21 +1,26 @@
-let slider = document.querySelector('.slider');
-let boxWidth = slider.offsetWidth / 3; // Breedte van de zichtbare slider
 let currentIndex = 0;
 
-function moveLeft() {
-  if (currentIndex > 0) {
-    currentIndex--;
-  } else {
-    currentIndex = 3; // Als we bij box 1 zijn en naar links klikken, ga naar box 6
+function moveLeft(sliderId) {
+    let slider = document.getElementById(sliderId);
+    let boxWidth = slider.offsetWidth / 3; // Breedte van de zichtbare slider
+  
+    if (currentIndex > 0) {
+      currentIndex--;
+    } else {
+      currentIndex = 3; // Als we bij box 1 zijn en naar links klikken, ga naar box 6
+    }
+    slider.style.transform = `translateX(-${currentIndex * boxWidth}px)`;
   }
-  slider.style.transform = `translateX(-${currentIndex * boxWidth}px)`;
-}
-
-function moveRight() {
-  if (currentIndex < 3) {
-    currentIndex++;
-  } else {
-    currentIndex = 0; // Als we bij box 6 zijn en naar rechts klikken, ga naar box 1
+  
+  function moveRight(sliderId) {
+    let slider = document.getElementById(sliderId);
+    let boxWidth = slider.offsetWidth / 3; // Breedte van de zichtbare slider
+  
+    if (currentIndex < 3) {
+      currentIndex++;
+    } else {
+      currentIndex = 0; // Als we bij box 6 zijn en naar rechts klikken, ga naar box 1
+    }
+    slider.style.transform = `translateX(-${currentIndex * boxWidth}px)`;
   }
-  slider.style.transform = `translateX(-${currentIndex * boxWidth}px)`;
-}
+  
