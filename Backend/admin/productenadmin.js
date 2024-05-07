973ea0d32bbcfcab9ea9d613068ea05276eb0c4d
WoutDepeuter <123627621+WoutDepeuter@users.noreply.github.com>
2024-05-07 12:23:42 +0200

    meer functies

---
+++ Backend/admin/productenadmin.js
@@ -0,0 +1,8 @@
+let filter = document.getElementById('filter-icoon');
+
+filter.addEventListener('onmousehover', changecolor);
+function changecolor(){
+
+    filter.innerHTML = "img src='../../frontend/Admin-interface/afbeeldingen/filter-icoon-hover.png' alt='filter icoon'"
+
+}
