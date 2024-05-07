bf399b678ec1b04af98f1bd287e64326cbedd2bb
WoutDepeuter <123627621+WoutDepeuter@users.noreply.github.com>
2024-05-07 10:55:19 +0200

    aanmaken return form nog geen opmaak

--- Backend/admin/return.js
+++ Backend/admin/return.js
@@ -16,23 +16,34 @@ for(let i = 0; i < annuleer.length; i++) {
 
 
 
+
 function bevestigRetour() {
-console.log('bevestigRetour');
+let klant = this.closest('.klantinfo');
+klant.innerHTML = `
+
+    <div class="returnrform">
+        <h1>Product ontvangen</h1>
+        <form id="returnForm">
+            <input type="radio" name="schade" id="schade1">
+            <label for="schade1">Schade</label><br>
+            <input type="radio" name="schade" id="schade2">
+            <label for="schade2">Schade2</label><br>
+            <input type="text" name="schade" id="damage">
+            <label for="damage">Is er schade</label><br>
+            <button type="submit">Submit</button>
+        </form>
+        <h1>ID 144 </h1>
+    </div>
+`;
 }
 function annuleerRetour() {
-    
+    //maak een div om klant info weg te halen
     let klant =  this.closest('.klantinfo');
-    
-    klant.innerHTML = ""
-    
+    // zoekt de klant info dichtbij de button die geduwd is en verwijderd deze
+    klant.innerHTML = "<div><h1 class='nietterug'>product niet teruggebracht</h1><a href='../../frontend/Admin-interface/HoofdMenuAdmin.html'>Terug naar home</a></div>";
+    //geeft een medling dat het product niet teruggebracht is en een link naar de home pagina
+
+    //moet later nog database functionaliteit bij enzo
 }
 
-function extraklant()
-{
- let ontvangen = document.getElementsByClassName('ontvangen');
- for (let i = 0; i < 10; i++) {
-    let klant = document.createElement('div');
-    klant.classList.add('klantinfo');
 
-    
-}
