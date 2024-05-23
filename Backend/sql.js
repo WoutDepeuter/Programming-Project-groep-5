4c5c74976db3b99f4c446650846858dca5d33d60
WoutDepeuter <123627621+WoutDepeuter@users.noreply.github.com>
2024-05-23 14:43:22 +0200

    ejs

--- Backend/sql.js
+++ Backend/sql.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const mysql = require("mysql2");
-const app = express();
-const env = require("dotenv").config().parsed;
-const path = require("path"); // Add this line to import the path module
+  const express = require("express");
+  const mysql = require("mysql2");
+  const app = express();
+  const env = require("dotenv").config().parsed;
+  const path = require("path"); // Add this line to import the path module
 
 const pool = mysql.createPool({
   host: env.HOST,
@@ -13,6 +13,30 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
 });
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: false,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'b6C2EyDcMOAAhiYy6BOwawnRtlOzC7Ok',
+  issuerBaseURL: 'https://ehbloan.eu.auth0.com'
+};
+const { requiresAuth } = require('express-openid-connect');
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// // req.isAuthenticated is provided from the auth router
+// app.get('/', (req, res) => {
+//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+// });
+
 
 // Set EJS as the view engine
 app.set("view engine", "ejs");
@@ -88,20 +112,6 @@ app.get("/xr", (req, res) => {
     }
   );
 });
-<<<<<<< HEAD
-app.get('/video', (req, res) => {
-    pool.query('SELECT * FROM PRODUCTMODEL WHERE Cat_ID = ?', [4], (err, results) => {
-        if (err) {
-            console.error('Error fetching products:', err);
-            // Render an error page or handle the error appropriately
-            res.status(500).send('Internal Server Error');
-            return;
-            
-
-        }
-        res.render('productenadmin/productenvideo', { products: results });
-    });
-=======
 app.get("/video", (req, res) => {
   pool.query(
     "SELECT * FROM PRODUCTMODEL WHERE Cat_ID = ?",
@@ -109,14 +119,15 @@ app.get("/video", (req, res) => {
     (err, results) => {
       if (err) {
         console.error("Error fetching products:", err);
-        // Render an error page or handle the error appropriately
         res.status(500).send("Internal Server Error");
         return;
       }
+      res.render("productenadmin/productenvideo",{ products: results });
     }
   );
->>>>>>> 6675921dc9e3f0819bb7f10ae01b8b5cce2d347f
 });
+
+
 
 app.get("/homepageadmin", (req, res) => {
   res.render("Admin-interface/HoofdMenuAdmin");
@@ -135,6 +146,7 @@ app.get("/catalogusvaria", (req, res) => {
 });
 
 app.get("/catalogusvideo", (req, res) => {
+
   res.render("User-interface/catalogus/video-catalogus");
 });
 
@@ -149,9 +161,9 @@ app.get('/homescreen', (req,res) => {
 
 
 
-app.get("/login", (req, res) => {
-  res.render("User-interface/Login/login");
-});
+// app.get("/login", (req, res) => {
+//   res.render("User-interface/Login/login");
+// });
 
 app.get("/signUp", (req, res) => {
   res.render("User-interface/Login/signUp");
