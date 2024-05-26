28b2529d72665a4673f3a74753b88d9e0fb7a869
WoutDepeuter <123627621+WoutDepeuter@users.noreply.github.com>
2024-05-27 00:12:57 +0200

    linksaanpassen

--- Frontend/public/js/admin/productenadmin.js
+++ Frontend/public/js/admin/productenadmin.js
@@ -2,7 +2,5 @@ let filter = document.getElementById('filter-icoon');
 
 filter.addEventListener('onmousehover', changecolor);
 function changecolor(){
-
     filter.innerHTML = "img src='../../frontend/Admin-interface/afbeeldingen/filter_hover.png' alt='filter-icoon"
-
 }
