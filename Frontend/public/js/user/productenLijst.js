a4ae2ca0378491c857b8372b667b6d755442d9b7
WoutDepeuter <123627621+WoutDepeuter@users.noreply.github.com>
2024-05-28 15:02:42 +0200

    test

--- Frontend/public/js/user/productenLijst.js
+++ Frontend/public/js/user/productenLijst.js
@@ -1,31 +1,35 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const reserveButtons = document.querySelectorAll('#reserveren');
+  document.addEventListener('DOMContentLoaded', () => {
+    const reserveButtons = document.querySelectorAll('.reserveren');
     const productPopup = document.getElementById('productPopup');
     const closePopupButton = document.getElementById('closePopup');
     const productDetails = document.getElementById('productDetails');
-  
+
     reserveButtons.forEach(button => {
       button.addEventListener('click', (e) => {
         e.preventDefault();
+        const productId = button.getAttribute('data-product-id');
+        const productName = document.getElementById(`naam${productId}`).textContent;
+        const productDescription = button.closest('.box').querySelector('.naam-en-besch p').textContent;
 
+        console.log(productId)
         productDetails.innerHTML = `
-        <div class="popup-box">
-        <!-- Content of the box goes here -->
-      </div>
+          <div class="popup-box">
+            <h2>${productName}</h2>
+            <p>${productDescription}</p>
+          </div>
         `;
-  
+
         productPopup.style.display = 'flex';
       });
     });
-  
+
     closePopupButton.addEventListener('click', () => {
       productPopup.style.display = 'none';
     });
-  
+
     productPopup.addEventListener('click', (e) => {
       if (e.target === productPopup) {
         productPopup.style.display = 'none';
       }
     });
   });
-  
