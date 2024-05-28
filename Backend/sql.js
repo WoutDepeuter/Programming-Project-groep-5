ed9028ef9f4fbe780a1dc4d94d26a34bb2ce0b89
mathias782 <145474322+mathias782@users.noreply.github.com>
2024-05-28 14:44:02 +0200

    HomeScreen werkende preview
    De HomeScreen heeft nu een werkende catalogus voor elke categorie

--- Backend/sql.js
+++ Backend/sql.js
@@ -124,8 +124,25 @@ app.post("/addProduct", upload.single('productFoto'), async (req, res) => {
 
 // User-interface-------------------------------------------------------------------------------
 
-app.get('/homescreen', (req, res) => {
-  res.render('User-interface/homescreen');
+app.get('/homescreen', async (req, res) => {
+  try {
+    const [audioSlider] = await pool.query("SELECT * FROM PRODUCTMODEL WHERE Cat_ID = ?", [1]);
+    const [belichtingSlider] = await pool.query("SELECT * FROM PRODUCTMODEL WHERE Cat_ID = ?", [2]);
+    const [variaSlider] = await pool.query("SELECT * FROM PRODUCTMODEL WHERE Cat_ID = ?", [3]);
+    const [videoSlider] = await pool.query("SELECT * FROM PRODUCTMODEL WHERE Cat_ID = ?", [4]);
+    const [xrSlider] = await pool.query("SELECT * FROM PRODUCTMODEL WHERE Cat_ID = ?", [5]);
+    
+    res.render("User-interface/homescreen", {
+      audioSlider,
+      belichtingSlider,
+      variaSlider,
+      videoSlider,
+      xrSlider
+    });
+  } catch (err) {
+    console.error("Error fetching products:", err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.get('/verlenging', (req, res) => {
