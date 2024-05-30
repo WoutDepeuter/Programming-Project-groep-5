2f25a3619ab559ca1c172c2e71159660432aca8d
IsmaelCH <ismael.charia.jimenez@student.ehb.be>
2024-05-30 12:16:27 +0200

    Popup (html,css,js)
    de popup is aangemaak, moet worden gekoppeld aan onze website.

---
+++ Frontend/public/js/admin/wijzigingProduct.js
@@ -0,0 +1,14 @@
+document.getElementById("openPopup").addEventListener("click", function () {
+    document.getElementById("popup").style.display = "block";
+  });
+  
+  document.getElementById("closeButton").addEventListener("click", function () {
+    document.getElementById("popup").style.display = "none";
+  });
+  
+  window.addEventListener("click", function (event) {
+    if (event.target == document.getElementById("popup")) {
+      document.getElementById("popup").style.display = "none";
+    }
+  });
+  
