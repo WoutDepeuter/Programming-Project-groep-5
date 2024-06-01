802c74977fb20d922a8433a5db013429805fe4eb
tristanvong <146710405+tristanvong@users.noreply.github.com>
2024-06-01 20:50:13 +0200

    productenadmin.js verwijderd (wordt niet gebruikt)

--- Frontend/public/js/admin/productenadmin.js
+++
@@ -1,7 +0,0 @@
-let filter = document.getElementById('filter-icoon');
-
-filter.addEventListener('onmousehover', changecolor);
-function changecolor(){
-    filter.innerHTML = "img src='../../frontend/Admin-interface/afbeeldingen/filter_hover.png' alt='filter-icoon"
-}
-
